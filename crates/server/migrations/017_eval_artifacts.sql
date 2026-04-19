15d5c58aa1b3f6ccd9cbd5210d21df1a2400b322
Mykhailo Chalyi <mike@chaliy.name>
2026-04-19 04:54:51 +0000

    chore(release): prepare v0.8.16

--- crates/server/migrations/017_eval_artifacts.sql
+++
@@ -1,7 +0,0 @@
--- Add artifact specs to eval cases and collected artifact payloads to eval case results.
-
-ALTER TABLE eval_cases
-    ADD COLUMN artifacts JSONB;
-
-ALTER TABLE eval_case_results
-    ADD COLUMN artifacts JSONB;
